2a78e15f00bcf03ff535c39e404a558f57650946
Dogukan <reatline@gmail.com>
2021-06-12 16:14:22 +0300

    Add project files.

---
+++ RabbitMQConsumerApp/Consumer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQSample
+{
+    public class Consumer
+    {
+        private readonly RabbitMQService _rabbitMQService;
+
+        public Consumer(string queueName)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    // Received event'i sürekli listen modunda olacaktır.
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+
+                        Console.WriteLine("{0} isimli queue üzerinden gelen mesaj: \"{1}\"", queueName, message);
+                    };
+
+                    channel.BasicConsume(queueName, true, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQConsumerApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RabbitMQSample
+{
+    class Program
+    {
+        private static readonly string _queueName = "DOGUKANOZTURK";
+        private static Consumer _consumer;
+
+        static void Main(string[] args)
+        {
+            _consumer = new Consumer(_queueName);
+        }
+    }
+}
